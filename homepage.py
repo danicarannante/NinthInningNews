bb724a933ffd9458ec2cc55da71875d888275e48
Dani Carannante <107160224+danicarannante@users.noreply.github.com>
2024-07-20 11:33:54 -0400

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pandas as pd
 from pybaseball import standings,statcast
 from variables import get_league_data
+from pybaseball import cache
+
+cache.enable()
 
 st.set_page_config(
     page_title="MLB Data Explorer"
 )
-
 
 
 selected_year = st.sidebar.selectbox('Year', list(reversed(range(2019,2025))))
@@ -21,7 +23,6 @@ title = f"""
 """
 st.markdown(title, unsafe_allow_html=True)
 
-
 col1, col2 = st.columns(2)
 for league in current_standings[:3]:
     col1.write(league)
@@ -31,12 +32,10 @@ for league in current_standings[3:]:
 teams = sorted(set(team for table in current_standings for team in table['Tm']))
 st.session_state['teams'] = teams
 
-
-# df = pd.read_csv(f'{selected_year}.csv')
-# print(df) 
 if 'league_data' not in st.session_state or st.session_state['data'] is False:
     print('loading league data.....')
-    # st.session_state['league_data'] =   pd.read_csv(f'{selected_year}.csv')
+    #st.session_state['league_data'] =   pd.read_csv(f'{selected_year}.csv')
     st.session_state['league_data'] = get_league_data()
     st.session_state['data'] = True
     print('finished....')
+    st.write()
